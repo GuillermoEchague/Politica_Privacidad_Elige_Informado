6555dcf5752fdbdf30dcdd23ac29261c4c929f4d
Guillermo Echague <46538216+GuillermoEchague@users.noreply.github.com>
2022-07-08 23:20:44 -0400

    Politicas APP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>POLÍTICA DE PRIVACIDAD PARA APLICACIÓN MÓVILE ELIGE INFORMADO</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nuestra política de privacidad se aplica al uso de las aplicación
+          informáticas ELIGE INFORMADO para dispositivos móviles Android.
+          Nuestra política de privacidad incluye las condiciones de uso de las
+          aplicaciones. La utilización de estas implica su aceptación y sin
+          reservas a todas y cada una de las disposiciones incluidas en estas.
+          En caso de no aceptar a dichas disposiciones no debe utilizar las
+          aplicaciones antes mencionadas. Nos reservamos el derecho a modificar,
+          total o parcialmente esta Política de Privacidad. Se aconseja que
+          consulte esta Política de Privacidad periódicamente para cualquier
+          cambio, ya que el uso continuado será considerado como la aprobación
+          de todos los cambios.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>1. información que se obtiene la aplicación y su utilización</h1>
+        <p>
+          a. Usuario proporcionó información La aplicación obtiene la
+          información que usted proporciona cuando se descarga y se registra la
+          aplicación. Para utilizar algunos de los servicios o acceder a
+          determinados contenidos, deberá proporcionar previamente ciertos datos
+          de carácter personal, que solo serán utilizados para el propósito que
+          fueron recopilados Cuando se registre con nosotros y utilice la
+          aplicación, por lo general, deberá proporcionar (a) su nombre,
+          dirección de correo electrónico, nombre de usuario, contraseña y otra
+          información de registro, (b) la información relacionada con la
+          transacción, por ejemplo, cuando usted entrega su opinión, responder a
+          cualquier oferta, o descarga una aplicación de nosotros, (c) la
+          información que usted nos proporciona cuando se comunica con nosotros
+          en busca de ayuda , (d) la información de tarjetas de crédito para la
+          compra y uso de la aplicación , y (e) la información que introduce en
+          nuestro sistema al utilizar la aplicación, tales como información de
+          contacto y la información de gestión de proyectos . b. Protección de
+          datos personales Cómo principio general, estas aplicaciones no
+          comparten ni revelan información obtenida, excepto cuando haya sido
+          autorizada por usted, o en los siguientes casos: (a) Cuando le sea
+          requerido por una autoridad competente y previo el cumplimiento del
+          trámite legal correspondiente y (b) Cuando a juicio de esta aplicación
+          sea necesario para hacer cumplir las condiciones de uso y demás
+          términos de la aplicación, o para salvaguardar la integridad de los
+          demás usuarios o de la aplicación. Deberá estar consciente de que si
+          usted voluntariamente revela información personal en línea en un área
+          pública, esa información puede ser recogida y usada por otros.
+          Nosotros no controlamos las acciones de nuestros visitantes y
+          usuarios. También podemos utilizar la información que nos ha
+          facilitado para que nos comuniquemos con usted de vez en cuando para
+          ofrecerle información importante, noticias y promociones de marketing.
+          c. Información recopilada automáticamente Además, la aplicación puede
+          recopilar cierta información automáticamente, incluyendo, pero no
+          limitado a el tipo de dispositivo móvil que utilice, los dispositivos
+          móviles de identificación única del dispositivo, la dirección IP de su
+          dispositivo móvil, el sistema operativo móvil, el tipo de móvil
+          navegadores de Internet que utiliza, y la información sobre el modo de
+          usar la aplicación. ¿La aplicación recopila información de ubicación
+          precisa en tiempo real del dispositivo ? Esta aplicación no recoge
+          información precisa sobre la ubicación de su dispositivo móvil. ¿Los
+          terceros pueden ver y/o tener acceso a la información obtenida por la
+          aplicación? Sí. Vamos a compartir su información con terceros sólo en
+          las formas que se describen en esta declaración de privacidad y que
+          por lo tanto hayan sido autorizadas por los usurios. Podemos revelar
+          información que los usuarios proveen y se recaudan automáticamente por
+          la aplicación: • Cuando creemos de buena fe que dicha divulgación es
+          necesaria para proteger nuestros derechos, la protección de su
+          seguridad o la seguridad de otros, investigar el fraude, o para
+          responder a una solicitud del gobierno; • Con nuestros proveedores de
+          servicios de confianza que trabajan en nuestro nombre, no tienen un
+          uso independiente de la información que divulgamos a ellos, y han
+          acordado adherirse a las reglas establecidas en esta declaración de
+          privacidad. • Si ELIGE INFORMADO está involucrada en una fusión,
+          adquisición o venta de la totalidad o una parte de sus activos, se le
+          notificará por correo electrónico y/o un aviso destacado en nuestra
+          APP de cualquier cambio en la propiedad o la utilización de esta
+          información, así como las opciones que usted puede tener con respecto
+          a esta información. • A los anunciantes y redes publicitarias de
+          terceros y las empresas de análisis como se describe en la siguiente
+          sección.
+        </p>
+
+        <h2>Recopilación automática de datos y publicidad</h2>
+        <p>
+          Podemos trabajar con las empresas de análisis para ayudar a entender
+          cómo se está utilizando la aplicación, tales como la frecuencia y
+          duración de uso. Trabajamos con los anunciantes y redes publicitarias
+          de terceros, que tienen que saber la forma de interactuar con la
+          publicidad prevista en la solicitud que nos ayuda a mantener el costo
+          de la aplicación GRATIS. Los anunciantes y redes publicitarias usan
+          parte de la información recopilada por la Aplicación, incluyendo, pero
+          no limitado a él ID único de identificación de su dispositivo móvil y
+          su número de teléfono móvil. Para proteger el anonimato de esta
+          información, utilizamos una tecnología de encriptación para asegurar
+          que estos terceros no pueden identificar personalmente. Estas terceras
+          partes también pueden obtener información anónima acerca de otras
+          aplicaciones que haya descargado en su dispositivo móvil, los sitios
+          web para móviles que visita, la información de ubicación no precisa
+          (por ejemplo , el código postal), y otra información de ubicación no
+          precisa con el fin de ayudar a analizar y servir la publicidad
+          dirigida anónima sobre la aplicación y en otros lugares. También
+          podemos compartir versiones cifradas de la información que ha
+          proporcionado a fin de que nuestros socios para anexar otra
+          información disponible acerca de usted para el análisis o el uso
+          relacionado con la publicidad. Si desea darse de baja del uso de
+          terceras partes de este tipo de información para ayudar a servir la
+          publicidad dirigida, por favor visite la sección titulada "Opt - out"
+          a continuación. ¿Cuáles son mis derechos Opt -­‐ out? Existen
+          múltiples opciones de Opt - out para los usuarios de esta aplicación:
+          Opt - out de toda la información recogida por la desinstalación de la
+          aplicación: Puede detener toda la colección de información por la
+          aplicación de forma sencilla mediante la desinstalación de la
+          aplicación. Puede utilizar los procesos estándar de desinstalación que
+          disponibles como parte de su dispositivo móvil o por el mercado de
+          aplicaciones móviles o la red. También puede solicitar darse de baja a
+          través de correo electrónico, en guillermo.echaguea@gmail.com. Opt -
+          out por el uso de la información para servir publicidad orientada por
+          los anunciantes directos y/o anunciantes de red de terceros: es
+          posible que en cualquier realizar opt - out de los datos a los cuáles
+          el usuario nos permite tener acceso a través del ícono configuración
+          ubicado en el borde superior derecho de cada aplicación.
+        </p>
+        <h1>Política de Retención de Datos, Gestión de la Información</h1>
+        <p>
+          Vamos a conservar datos de cada usuario proporcionada por el tiempo
+          que se utiliza la aplicación y durante un plazo razonable. Vamos a
+          conservar la información recopilada automáticamente hasta por 3 AÑOS
+          y, posteriormente, se puede guardar definitivamente. Si desea que
+          eliminemos los datos del usuario que han sido proporcionados a través
+          de la aplicación, póngase en contacto con nosotros en
+          guillermo.echaguea@gmail.com y le responderemos en un plazo razonable.
+          Tenga en cuenta que algunos o todos los datos que proporcionaron los
+          usuarios pueden ser requeridos para que la aplicación funcione
+          correctamente.
+        </p>
+        <h2>Niños</h2>
+        <p>
+          No utilizaremos la aplicación para obtener datos a niños menores de 14
+          años. Si un padre o tutor tiene conocimiento de que su hijo nos ha
+          proporcionado información sin su consentimiento, él o ella debe
+          ponerse en contacto con nosotros a guillermo.echaguea@gmail.com. Vamos
+          a eliminar dicha información de nuestros archivos dentro de un plazo
+          razonable.
+        </p>
+
+        <h2>Seguridad</h2>
+        <p>
+          Estamos preocupados por salvaguardar la confidencialidad de su
+          información. Proveemos salvaguardas físicas, electrónicas y de
+          procedimiento para proteger la información que procesamos y mantener.
+          Por ejemplo, limitamos el acceso a esta información a los empleados y
+          contratistas autorizados que necesitan conocer dicha información para
+          operar, desarrollar o mejorar nuestra aplicación. Tenga en cuenta que,
+          aunque nos esforzamos por proporcionar seguridad razonable de
+          información que procesamos y mantenemos, ningún sistema de seguridad
+          puede prevenir todas las brechas de seguridad potenciales.
+        </p>
+
+        <h2>Su Consentimiento</h2>
+        <p>
+          Mediante el uso de la aplicación, usted acepta nuestro procesamiento
+          de sus datos en los términos de esta Política de Privacidad de vez en
+          cuando modificado por nosotros. "Proceso", significa el uso de cookies
+          en un dispositivo de computadora y/o portátil o tocar la información
+          de cualquier manera, no limitado a recoger, almacenar, borrar,
+          utilizar la combinación y la divulgación de la información, todas las
+          actividades que se llevarán a cabo en CHILE. Si reside fuera de CHILE
+          su información será transferida, procesada y almacenada allí en CHILE
+          en base a las normas de privacidad.
+        </p>
+
+        <h2>Contáctenos</h2>
+        <p>
+          Si usted tiene alguna pregunta respecto a la privacidad durante el uso
+          de la aplicación, o si tiene preguntas sobre nuestras prácticas, por
+          favor póngase en contacto con nosotros por correo electrónico a
+          guillermo.echaguea@gmail.com.
+        </p>
       </header>
     </div>
   );
